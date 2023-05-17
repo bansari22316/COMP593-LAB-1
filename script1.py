eaa1bbdb949b46899f25d705c849539c7543ccd7
pujpat <pujpat@flemingcollege.ca>
2023-05-17 16:27:10 -0400

    changes made to both scripts
    deleted print() from script 1 and added 2 print() statements in script 2.

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #print lines from zen of python
 print("Hello World")
-print("I want to live my dreams.")
--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 #print lines from zen of python
 print("maths is my fav sub.")
 print("I can do it at any time.")
+print("Night is the best time to do maths.")
+print("According to me hehehehe,")
